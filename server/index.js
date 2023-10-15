efff31dbe604c0791cf843eb0b3fc0db1b04f8ad
SahilPandhade <sahilpandhade@rediffmail.com>
2023-10-15 17:34:26 +0530

    deployment changes

--- server/index.js
+++ server/index.js
@@ -31,10 +31,11 @@ async function startApolloServer() {
                 ApolloServerPluginLandingPageDisabled()
         ],
     })
-
+    console.log(path.resolve(path.join(__dirname,"..",".."),'client','dist','index.html'))
     if(process.env.NODE_ENV == "production") {
         app.use(express.static('client/build'))
         app.get("*",(req,res)=>{
+            
             res.sendFile(path.resolve(path.join(__dirname,"..",".."),'client','dist','index.html'))
         })
     }

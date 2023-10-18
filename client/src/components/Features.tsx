fb5ce8852e6aa8f7024666b2a0f92950dd3d7e95
SahilPandhade <sahilpandhade@rediffmail.com>
2023-10-18 16:00:18 +0530

    Added homepage banner

---
+++ client/src/components/Features.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef } from 'react'
+
+const features = [
+  {
+    id: 1,
+    title: 'Never forget to complete your tasks again',
+    text: 'Organize and priorities your tasks at ease.',
+  },
+  {
+    id: 2,
+    title: 'Ease of access',
+    text: 'Access and update your tasks quickly,anytime,anywhere.',
+  },
+  {
+    id: 3,
+    title: 'Always on the roll',
+    text: 'New features being added regularly so that you get a smooth user experience',
+  },
+]
+const Features = () => {
+  const featuresContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = featuresContainerRef.current;
+      if (element && isElementInViewport(element)) {
+        element.classList.remove('animate-slide-left')
+        setTimeout(() => {
+          element.classList.add('animate-slide-left');
+        }, 1)
+
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const isElementInViewport = (el: HTMLElement | null) => {
+    if (!el) return false;
+
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+  return (
+    <div className="container mt-5" ref={featuresContainerRef}>
+      <div className="row">
+        {
+          features.map((feature) => (
+            <div className="col-md-4 mb-4" key={feature.id}>
+              <div className="card feature-card bg-black text-white">
+                <div className="card-body">
+                  <h5 className="card-title">{feature.title}</h5>
+                  <p className="card-text">
+                    {feature.text}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+
+
+  )
+}
+
+export default Features
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import banner_image from '../assets/homepage.jpg'
+import Features from '../components/Features'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/authContext'
 const HomePage = () => {
+  const navigate = useNavigate()
+  const {user} = useContext(AuthContext)
+  const handleClick = ()=>{
+    if(!user){
+      navigate('/register')
+    }
+    else{
+      navigate('/dashboard')
+    }
+  }
   return (
-    <div>HomePage</div>
+    <>
+    <div>
+      <div
+        style={{
+          position: 'relative',
+          height: '650px',
+          width: '100%',
+          overflow: 'hidden',
+        }}
+      >
+        <img
+          src={banner_image}
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            filter: 'brightness(70%)',
+          }}
+        />
+        <div
+          className="d-flex flex-column justify-content-center align-items-center"
+          style={{
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          }}
+        >
+          <h1 className="text-white text-center mb-4">
+            Supercharge your productivity with TaskQL
+          </h1>
+          <button className="btn btn-lg shadow" style={{ backgroundColor: '#5adbb5' }} onClick={handleClick}>
+             {user ?'Go to my dashboard' : 'Start Now'} 
+          </button>
+        </div>
+      </div>
+    </div>
+    <Features/>
+    </>
+
   )
 }
 
